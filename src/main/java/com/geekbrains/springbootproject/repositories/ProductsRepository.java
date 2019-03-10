7b882c0a0d6e91fbb98721afaa40e8c01c4b1a97
anyuoyuna <anyuo.91@mail.ru>
2019-03-10 12:57:15 +0300

    Lesson8

--- src/main/java/com/geekbrains/springbootproject/repositories/ProductsRepository.java
+++ src/main/java/com/geekbrains/springbootproject/repositories/ProductsRepository.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface ProductsRepository extends CrudRepository<Product, Long> {
+public interface ProductsRepository extends PagingAndSortingRepository<Product, Long> {
     Page<Product> findAllByCostBetween(Pageable pageable, double minCost, double maxCost);
     Product findOneByTitle(String title);
 }
