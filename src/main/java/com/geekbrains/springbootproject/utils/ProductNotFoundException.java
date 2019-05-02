559117f6b51ca6ac244c53f58e18e3e3d176e63d
anyuoyuna <anyuo.91@mail.ru>
2019-05-02 15:50:56 +0300

    Lesson6

--- src/main/java/com/geekbrains/springbootproject/utils/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.springbootproject.utils;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
